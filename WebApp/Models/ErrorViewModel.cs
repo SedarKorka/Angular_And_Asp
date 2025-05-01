3c69b73f91bfad594c502b0fbc50a9c3cb03f94f
Mamadou Korka Diallo <lelevekorka@gmail.com>
2025-05-01 16:37:29 +0200

    Angular App

--- WebApp/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Angular_And_Asp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
